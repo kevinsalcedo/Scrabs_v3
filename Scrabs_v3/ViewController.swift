6faeb91ba53de854a90c52b8682e5c05806378a0
Kevin Salcedo <kevsalcedo@gmail.com>
2018-09-05 11:13:41 -0500

    Initial Commit

---
+++ Scrabs_v3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scrabs_v3
+//
+//  Created by Kevin Salcedo on 9/5/18.
+//  Copyright © 2018 Kevin Salcedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
